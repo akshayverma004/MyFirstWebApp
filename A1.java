c5aa0d7c71222b9befa077f110972b31381f832c
akshay <akshayverma004@gmail.com>
2026-05-14 01:51:40 +0530

    final A1.java

--- A1.java
+++ A1.java
@@ -5,8 +5,15 @@ public class A1
 	{
 	int a=10;
 	int b=5;
+<<<<<<< Updated upstream
 	int c,e;
 		System.out.println(c=a+b);
 		System.out.println(e=a/b);
+=======
+	int c,d;
+		System.out.println(c=a+b);
+		System.out.println(d=a-b);
+		
+>>>>>>> Stashed changes
 	}
 }
